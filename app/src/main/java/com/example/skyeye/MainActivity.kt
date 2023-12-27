4c8fc5b3c88893d43080b13a0aa8c2ed809e093a
Layton Berth <laytonbrth@gmail.com>
2023-12-27 19:58:18 +0100

    add map and basic navbar

--- app/src/main/java/com/example/skyeye/MainActivity.kt
+++ app/src/main/java/com/example/skyeye/MainActivity.kt
@@ -3,14 +3,38 @@ package com.example.skyeye
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.skyeye.ui.theme.SkyEyeTheme
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +46,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Kempie en Layton")
+                    MyScaffold()
                 }
             }
         }
@@ -30,17 +54,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MapView(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            Mapbox.getInstance(context)
+            val mapView = com.mapbox.mapboxsdk.maps.MapView(context)
+            val styleUrl = "https://api.maptiler.com/maps/basic-v2/style.json?key=OZkqnFxcrUbHDpJQ5a3K";
+            mapView.onCreate(null)
+            mapView.getMapAsync { map ->
+                // Set the style after mapView was loaded
+                map.setStyle(styleUrl) {
+                    map.uiSettings.setAttributionMargins(15, 0, 0, 15)
+                    // Set the map view center
+                    map.cameraPosition = CameraPosition.Builder()
+                        .target(LatLng(50.5, 4.47))
+                        .zoom(3.5)
+                        .bearing(2.0)
+                        .build()
+                }
+            }
+            mapView
+        }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SkyEyeTheme {
-        Greeting("Android")
-    }
 }
