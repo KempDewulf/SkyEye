2d0f1790bb235279c359b7bbf874efdf06f175a6
kemp.dewulf <kemp.dewulf@student.howest.be>
2023-12-28 19:30:43 +0100

    add navigation drawer

--- app/src/main/java/com/example/skyeye/MainActivity.kt
+++ app/src/main/java/com/example/skyeye/MainActivity.kt
@@ -7,22 +7,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -36,6 +56,15 @@ import com.example.skyeye.ui.theme.SkyEyeTheme
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +76,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Homescreen()
+                    Drawer()
                 }
             }
         }
@@ -55,10 +84,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Homescreen() {
+fun Drawer() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val items = listOf(
+        Pair(R.drawable.weather, "Check the weather"),
+        Pair(R.drawable.camera, "Search airplanes with AR"),
+        Pair(R.drawable.airplane, "See all aircraft types"),
+        Pair(R.drawable.runway, "See all airports"),
+        Pair(R.drawable.settings, "Settings")
+    )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = false,
+        drawerContent = {
+            ModalDrawerSheet(
+                drawerShape = RectangleShape,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(300.dp)
+            ) {
+                Spacer(Modifier.height(12.dp))
+                Row {
+                    IconButton(onClick = { scope.launch { drawerState.close() } }) {
+                        Icon(
+                            Icons.Rounded.ArrowBack,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(30.dp)
+                        )
+                    }
+                    TextButton(onClick = { /*TODO: send to register page*/ }) {
+                        Text(text = "Register", fontSize = 22.sp, modifier = Modifier.padding(start = 35.dp))
+                    }
+                    Box(
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                    ) {
+                        Text(text = "or", fontSize = 22.sp)
+                    }
+                    TextButton(onClick = { /*TODO: send to log in page*/ }) {
+                        Text(text = "Log in", fontSize = 22.sp)
+                    }
+
+                }
+                Divider(color = MaterialTheme.colorScheme.outlineVariant,thickness = 1.dp, modifier = Modifier.padding(start = 15.dp, end = 15.dp))
+                Spacer(Modifier.height(18.dp))
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        icon = { Icon(painterResource(item.first), contentDescription = null, modifier = Modifier.size(28.dp)) },
+                        label = { Text(item.second, fontSize = 16.sp) },
+                        selected = false,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                        },
+                        modifier = if (item.second == "Settings") {
+                            Modifier
+                                .padding(NavigationDrawerItemDefaults.ItemPadding)
+                                .padding(top = 380.dp)
+                        } else {
+                            Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        }
+                    )
+                }
+            }
+        },
+        content = {
+            Homescreen(drawerState, scope)
+        }
+    )
+}
+
+@Composable
+fun Homescreen(drawerState: DrawerState, scope: CoroutineScope) {
     Scaffold(
         topBar = {
-            TopBar()
+            TopBar(drawerState, scope)
         },
         bottomBar = {
             BottomAppBar()
@@ -72,14 +173,14 @@ fun Homescreen() {
 }
 
 @Composable
-fun TopBar() {
+fun TopBar(drawerState: DrawerState, scope: CoroutineScope) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        IconButton(onClick = { /*TODO*/ }) {
+        IconButton(onClick = { scope.launch { drawerState.open() } }) {
             Icon(Icons.Rounded.Menu, contentDescription = "menu", modifier = Modifier.size(32.dp))
         }
         IconButton(onClick = { /*TODO*/ }) {
@@ -90,7 +191,7 @@ fun TopBar() {
 
 @Composable
 fun BottomAppBar() {
-    var selectedItem by remember { mutableIntStateOf(0) }
+    var selectedItem by remember { mutableIntStateOf(-1) }
     val items = listOf(
         Pair(R.drawable.settings, "Settings"),
         Pair(R.drawable.weather, "Weather"),
