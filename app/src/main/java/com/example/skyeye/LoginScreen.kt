865f3e26f2f2c0a355eb35be1301a88b36daa2d6
kemp.dewulf <kemp.dewulf@student.howest.be>
2023-12-28 23:01:04 +0100

    bbb

--- app/src/main/java/com/example/skyeye/LoginScreen.kt
+++ app/src/main/java/com/example/skyeye/LoginScreen.kt
@@ -1,19 +1,35 @@
 package com.example.skyeye
 
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Face
-import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,56 +37,192 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skyeye.ui.theme.SkyEyeTheme
 
 @Preview
 @Composable
 fun LoginScreen() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-    ) {
-        Icon(Icons.Rounded.Face, contentDescription = null)
-        Text(text = "Log in to your SkyEye account")
-        Column {
-            Button(onClick = { /*TODO*/ }) {
-                Row {
-                    // Google Icon
-                    Text(text = "Log in with Google")
+    SkyEyeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .padding(top = 50.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_skyeye),
+                    contentDescription = null,
+                    modifier = Modifier.size(80.dp)
+                )
+                Text(text = "Log in to your SkyEye account", fontSize = 20.sp)
+                Column {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
+                        border = BorderStroke(0.5.dp, Color.DarkGray),
+                        modifier = Modifier
+                            .fillMaxWidth(0.75f)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.google__g__logo_1_),
+                                contentDescription = null,
+                            )
+                            Spacer(modifier = Modifier.width(80.dp))
+                            Text(
+                                text = "Log in with Google",
+                                textAlign = TextAlign.Start,
+                            )
+                        }
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF4285F4),
+                            contentColor = Color.White
+                        ),
+                        border = BorderStroke(0.5.dp, Color.DarkGray),
+                        modifier = Modifier
+                            .fillMaxWidth(0.75f)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.iconmonstr_facebook_1),
+                                contentDescription = null,
+                            )
+                            Spacer(modifier = Modifier.width(80.dp))
+                            Text(
+                                text = "Log in with Facebook",
+                                textAlign = TextAlign.Start,
+                            )
+                        }
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
+                        border = BorderStroke(0.5.dp, Color.DarkGray),
+                        modifier = Modifier
+                            .fillMaxWidth(0.75f)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.apple_logo_svgrepo_com),
+                                contentDescription = null,
+                                modifier = Modifier.size(20.dp)
+                            )
+                            Spacer(modifier = Modifier.width(80.dp))
+                            Text(
+                                text = "Log in with Google",
+                                textAlign = TextAlign.Start,
+                            )
+                        }
+                    }
                 }
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Row {
-                    // Google Icon
-                    Text(text = "Log in with Facebook")
-                }
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Row {
-                    // Google Icon
-                    Text(text = "Log in with Apple")
-                }
-            }
-        }
-        Text(text = "or with email")
-        Column {
-            var text by remember { mutableStateOf("") }
+                Text(text = "or with email")
+                Column(
+                    modifier = Modifier.fillMaxWidth(0.75f)
+                ) {
+                    var email by remember { mutableStateOf("") }
+                    var password by remember { mutableStateOf("") }
 
-            TextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Email") }
-            )
-        }
-        Row {
-            Text(text = "Don't have an account?")
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "Sign up")
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        modifier = Modifier
+                            .padding(bottom = 5.dp)
+                            .fillMaxWidth()
+                    )
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        label = { Text("Password") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Forgot password?")
+                        }
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Row {
+                            Text(text = "Log in")
+                        }
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Don't have an account?",
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(bottom = 1.dp) // Adjust the padding to fine-tune the alignment
+                    )
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Sign up")
+                    }
+                }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
