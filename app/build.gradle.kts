656919df51d1651a9ff367604f67248fb8fd4dd9
KempDewulf <kemp.dewulf@hotmail.com>
2024-05-03 19:10:50 +0200

    change package names

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.skyeye"
+    namespace = "howest.nma.skyeye"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.skyeye"
+        applicationId = "howest.nma.skyeye"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
