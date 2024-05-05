3a311bfdffdcadc1e8a8380bfbac8866c2c3ad59
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-05-05 03:11:49 +0200

    add weather sheet

---
+++ app/src/main/java/com/howest/skyeye/WeatherModal.kt
@@ -0,0 +1,117 @@
+package com.howest.skyeye
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import howest.nma.skyeye.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WeatherModal(onDismissRequest: () -> Unit) {
+    var activeItem by remember { mutableStateOf("No weather") }
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier.height(500.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .height(500.dp)
+                .padding(bottom = 50.dp)
+        ) {
+            item {
+                WeatherItem(R.drawable.none, "No weather", "No weather layer", activeItem == "No weather") {
+                    activeItem = "No weather"
+                }
+            }
+            item {
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.outlineVariant,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+            item {
+                WeatherItem(R.drawable.clouds, "Cloud coverage", "Global IR Satellite provides cloud cover displayed on the map.", activeItem == "Cloud coverage") {
+                    activeItem = "Cloud coverage"
+                }
+            }
+            item {
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.outlineVariant,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+            item {
+                WeatherItem(R.drawable.rain, "Rain", "Overview of the current precipitation on our live map.", activeItem == "Rain") {
+                    activeItem = "Rain"
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherItem(imageId : Int, title: String, text : String, active : Boolean, onClick: () -> Unit) {
+    Column {
+        Text(
+            text = title,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = onClick)
+        ) {
+            Image(
+                painter = painterResource(id = imageId),
+                contentDescription = "$title image",
+                contentScale = androidx.compose.ui.layout.ContentScale.Crop,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(all = 16.dp)
+                    .clip(RoundedCornerShape(4.dp)) // Clip the image to the rounded shape
+                    .border(
+                        width = if (active) 2.dp else 1.dp,
+                        color = if (active) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurfaceVariant,
+                        shape = RoundedCornerShape(4.dp)
+                    )
+            )
+            Text(
+                text = text,
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
